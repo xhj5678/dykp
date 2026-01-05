37a25ba06d409981fc15ab9dccf22f2ff38763dd
九尾 <2382972638@qq.com>
2026-01-06 01:50:55 +0800

    创建 register.php
    注册接口

---
+++ api/user/register.php
@@ -0,0 +1,57 @@
+<?php
+require '../../core/DB.php';
+require '../../core/Response.php';
+
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
+$inviteCode = trim($_POST['invite_code'] ?? '');
+
+if (!$username || !$password) {
+    Response::json([], 400, '参数不完整');
+}
+
+// 是否已存在
+$exists = DB::fetch("SELECT id FROM user WHERE username='$username'");
+if ($exists) {
+    Response::json([], 400, '账号已存在');
+}
+
+// 创建用户
+$pwd = md5($password);
+DB::query("
+    INSERT INTO user(username,password,created_at)
+    VALUES ('$username','$pwd',NOW())
+");
+
+$uid = DB::connect()->insert_id;
+
+// 生成自己的邀请码
+$myCode = $uid . strtoupper(substr(md5($uid.time()),0,4));
+DB::query("UPDATE user SET invite_code='$myCode' WHERE id=$uid");
+
+// 👉 绑定邀请关系（只能一次）
+if ($inviteCode) {
+
+    $parent = DB::fetch("SELECT id FROM user WHERE invite_code='$inviteCode'");
+
+    // 防止绑自己
+    if ($parent && $parent['id'] != $uid) {
+
+        // 防止重复绑定
+        $bind = DB::fetch("SELECT uid FROM invite WHERE uid=$uid");
+        if (!$bind) {
+            DB::query("
+                INSERT INTO invite(uid,parent_uid)
+                VALUES ($uid,{$parent['id']})
+            ");
+        }
+    }
+}
+
+// 登录态
+setcookie('uid', $uid, time()+86400*30, '/');
+
+Response::json([
+    'uid' => $uid,
+    'invite_code' => $myCode
+]);
